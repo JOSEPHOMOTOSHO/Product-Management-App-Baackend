7e52e9464c823f27a55ca3cd4343d61fec63924b
Omotosho Joseph <omotosho@ext.kobo360.com>
2022-07-28 16:17:46 +0100

    ji

--- firebase.config.js
+++ firebase.config.js
@@ -1,13 +1,15 @@
 const admin = require('firebase-admin');
 const serviceAccount = require("./tradedepot-c9f2c-firebase-adminsdk-fifci-9672f14418.json");
 
-exports.uploadToFireStore = (data) =>{
-    admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount),
-      });
-      const db = admin.firestore();
-      const productRef = db.collection("myproducts")
-      productRef.doc(data.id.toString()).set(data)
+exports.uploadToFireStore = (data) => {
+    if(!admin.apps.length){
+        admin.initializeApp({
+            credential: admin.credential.cert(serviceAccount),
+          });
+          const db = admin.firestore();
+          const productRef = db.collection("myproducts")
+          productRef.doc(data.id.toString()).set(data)
+    }   
 }
 
 
